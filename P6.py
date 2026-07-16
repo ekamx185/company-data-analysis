18a0cafa6f0385bd684e66797300c62a8f510bbf
ekamx185 <ekampreetsingh185@gmail.com>
2026-07-16 23:33:46 +0530

    Delete P6.py

--- P6.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-data = {
-    'Name': ['Ekam', 'Jaspinder' ,'Paggu' ,'Manshi'],
-    'Age' : [16,17,18],
-    'Salary' : [90000,70000,50000]
-}
-df = pd.DataFrame(data)
-print(df)
